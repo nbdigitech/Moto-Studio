62be621cbc1a9fb845d48f8acda6d13d45657bf6
tahsinchouhan <tahsinchouhan2000@gmail.com>
2022-11-14 13:14:45 +0530

    ...

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,27 +1,26 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 class WebDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
         <Head>
-          <Script
+          <script
             id="google-tag-manager"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-// })(window,document,'script','dataLayer','GTM-NSRNGJ3');`,
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NSRNGJ3');`,
             }}
-          ></Script>
+          ></script>
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSRNGJ3"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           ></noscript>
           <Main />
@@ -32,11 +31,3 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
   }
 }
 export default WebDocument;
-
-// <!-- Google Tag Manager -->
-// // <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-// // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-// // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-// // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-// // })(window,document,'script','dataLayer','GTM-NSRNGJ3');</script>
-// <!-- End Google Tag Manager -->
